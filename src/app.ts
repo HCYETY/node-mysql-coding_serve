0cbb1e41e34c0d54d00f928d3f244089f6afed4a
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-09-16 18:33:49 +0800

    登录模块初步整合

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,17 @@ createConnection ()
     const app = new Koa()
 
     const login = require('./middleware/login')
-    const register = require('./middleware/register')
     
+    // 处理跨域
     app.use(cors())
+    // 处理 post 请求的参数
       .use(bodyParser())
     // app.use(ctx => {
     //   if(ctx.path === 'login') {
     //     ctx.body = 
     //   }
     // })
-    app.use(login)
-    app.use(register)
+      .use(login)
     
     app.listen(3001, () => {
     console.log('网站服务器启动成功，请访问 http://localhost:3001')
--- src/middleware/login.ts
+++ src/middleware/login.ts
@@ -3,30 +3,39 @@ import { getManager } from "typeorm"
 import User from '../entity/User'
 
 module.exports = async (ctx:Context, next:any) => {
-  const url = ctx.url
-  const method = ctx.method
-  if (method === 'POST' && url === '/login') {
-    const {account, password} = ctx.request.body
-    console.log('@登录账号', account, typeof account)
-    console.log('@登录密码', password, typeof password)
-
-    const userRepository = getManager().getRepository(User)
-    let saveUsers = await userRepository.findOne({where:{account}})
-    console.log('$用户信息', saveUsers)
-    // console.log(saveUsers.account, typeof saveUsers.account)
-    // console.log(saveUsers.password, typeof saveUsers.password)
-    let res
-    if (saveUsers != null) {
-      if (saveUsers.password == password) {
-        res = {data:saveUsers, message:'登录成功', status: true, code:200}
-      } else {
-        res = {message:'密码错误', status: false, code:401}
-      }
-    } else {
-      res = {message:'该用户不存在，请先注册', status: false, code:422}
+  const url = ctx.url, method = ctx.method
+  const {account, password} = ctx.request.body
+  const userRepository = getManager().getRepository(User)
+  const saveUsers = await userRepository.findOne({where:{account}})
+  let res
+  if (method === 'POST') {
+    if (url === '/login') {
+      try{
+        if (saveUsers != null) {
+          if (saveUsers.password === password) {
+            res = {data:saveUsers, message:'登录成功', status: true}
+          } else {
+            res = {message:'密码错误', status: false}
+          }
+        } else {
+          res = {message:'该用户不存在，请先注册', status: false}
+        }
+      } catch (err) { console.log(err) }
+    } else if (url === '/register') {
+      try{
+        if (saveUsers === undefined) {
+          const newUser = new User()
+          newUser.account = account
+          newUser.password = password
+          await userRepository.save(newUser)
+          // await userRepository.save({account, password})
+          res = {status:true, message:'注册成功', data:newUser}
+        } else{
+          res = {status:false, message:'用户已存在，请注册新的邮箱'}
+        }
+      } catch(err) { console.log(err) }
     }
-    console.log(res)
-    ctx.body = res
   }
+  ctx.body = res 
   next()
 }
