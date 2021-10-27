54b21c55b37b71e0a106818b20d394140145f0a3
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-10-27 21:33:41 +0800

    保存候选人的试题答案

--- src/middleware/paper/candidateInform.ts
+++ src/middleware/paper/candidateInform.ts
@@ -8,26 +8,40 @@ import nodemail from '../../../sendEmail.js';
 export default async (ctx:Context) => {
   const req = ctx.request.body;
   const paperName = req.paper;
+  const testName = req.test;
   const candidateRepository = getRepository(Candidate);
-  const candidateInform = await candidateRepository.find({ paper: paperName });
-  const candidateEmail = candidateInform[0].email;
-  const testPaperRepository = getRepository(TestPaper);
-  const interviewerEmail = (await testPaperRepository.findOne({ paper: paperName })).interviewer;
+  console.log(req)
   
-  if (paperName && req.sign === true) {
-    for (let ch of candidateInform) {
-      ch.over = true;
-      await candidateRepository.save(ch);
-    }
-    
-    const mail = {
-      from: '1164939253@qq.com',
-      to: interviewerEmail,
-      subject: '在线编程笔试平台',
-      text:`候选人${ candidateEmail }的试卷：${ paperName }已提交，您可进入该网站 http://www.syandeg.com 查看试卷并批阅!`
-    };
-    nodemail(mail);
+  if (paperName) {
+    const candidateInform = await candidateRepository.find({ paper: paperName });
+    if (req.sign === true) {
+      const candidateEmail = candidateInform[0].email;
+      const testPaperRepository = getRepository(TestPaper);
+      const interviewerInform = await testPaperRepository.findOne({ paper: paperName });
+      const interviewerEmail = interviewerInform.interviewer;
+
+      for (let ch of candidateInform) {
+        ch.over = true;
+        await candidateRepository.save(ch);
+      }
+      
+      const mail = {
+        from: '1164939253@qq.com',
+        to: interviewerEmail,
+        subject: '在线编程笔试平台',
+        text:`候选人${ candidateEmail }的试卷：${ paperName } 已提交，您可登录该网站 http://www.syandeg.com 查看试卷并批阅!`
+      };
+      nodemail(mail);
+    } 
+    const obj = { status: true, candidateInform };
+    ctx.body = new responseClass(200, '试卷已提交，之后不可再编辑', obj);
+  } else if (testName) {
+    const candidateInform = await candidateRepository.findOne({ test_name: testName});
+    const candidateEmail = candidateInform.email;
+    const paperName = candidateInform.paper;
+    const candidateTest = await candidateRepository.findOne({ where: {email: candidateEmail, paper:paperName, test_name: testName }});
+    candidateTest.program_answer = req.code;
+    await candidateRepository.save(candidateTest);
+    ctx.body = new responseClass(200, '试题答案提交成功', );
   }
-  const obj = { status: true, candidateInform };
-  ctx.body = new responseClass(200, '试卷已提交，之后不可再编辑', obj);
 }
